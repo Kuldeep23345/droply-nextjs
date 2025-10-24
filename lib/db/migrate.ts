962c8851e64cebd1130a9ef8894dd340de575376
Kuldeep <kunalrajput67718@gmail.com>
2025-10-24 14:15:44 +0530

    added custom mirarations in drizzle

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,11 +1,26 @@
-import { migrate } from "drizzle-orm/neon-serverless/migrator";
+import { migrate } from "drizzle-orm/neon-http/migrator";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
   throw new Error("Database url is not set in .env");
 }
+
+async function runMigration() {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const db = drizzle(sql);
+    await migrate(db, { migrationsFolder: "./drizzle" });
+
+    console.log("all migrations are successfully done");
+  } catch (error) {
+    console.log("error while migrations ");
+    process.exit(1);
+  }
+}
+
+runMigration();
